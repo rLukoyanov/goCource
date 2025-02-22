5b8e021f7a99d52b1ce78f6b467059ec3b3ddeda
Ruslan Lukoyanov <ruslan.ru226@mail.ru>
2025-02-22 13:06:45 +0400

    lesson 125

--- maps.go
+++ maps.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
-	websites := map[string]string{
-		"google":              "https://google.com",
-		"amazon web services": "https://aws.com",
+	userNames := make([]string, 2, 5)
+	fmt.Println(len(userNames), cap(userNames))
+
+	userNames = append(userNames, "Max")
+
+	fmt.Println(userNames)
+
+	coursesRatings := make(floatMap, 3)
+
+	coursesRatings["go"] = 4.7
+	coursesRatings["react"] = 4.8
+	coursesRatings["angular"] = 4.7
+	coursesRatings["node"] = 4.8
+
+	coursesRatings.output()
+
+	// fmt.Println(coursesRatings)
+
+	for index, value := range userNames {
+		fmt.Println(index, value)
 	}
 
-	fmt.Println(websites["google"])
-	websites["linkedIn"] = "https://linkedin.com"
-	fmt.Println(websites)
-
-	delete(websites, "google")
-	fmt.Println(websites)
+	for key, value := range coursesRatings {
+		fmt.Println(key, "_", value)
+	}
 }
